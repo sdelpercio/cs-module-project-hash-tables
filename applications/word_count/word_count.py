ffad14792a3db9a79f5d8e5ff3444d0acc22392c
Shawn DelPercio <sdelperc@gmail.com>
2020-07-08 15:52:57 -0400

    implemented word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,27 @@
 def word_count(s):
-    # Your code here
+    s = s.split()
+    tally = {}
 
+    if not s:
+        return tally
+
+    for word in s:
+        stripped = "".join(ch for ch in word if ch.isalpha()
+                           or ch == "'").lower()
+        if stripped == '':
+            continue
+
+        if stripped in tally:
+            tally[stripped] += 1
+        else:
+            tally[stripped] = 1
+
+    return tally
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
