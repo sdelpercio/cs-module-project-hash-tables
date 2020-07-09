27fe8cef285343e2024a5e9ac571b43e6cf75b51
Shawn DelPercio <sdelperc@gmail.com>
2020-07-09 19:44:25 -0400

    implemented sumdiff

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -11,11 +11,40 @@ q = (1, 3, 4, 7, 12)
 def f(x):
     return x * 4 + 6
 
+matches = []
+simple_solns = {}
+add_solns = {}
+sub_solns = {}
 
 def sumdiff(nums):
-    # start dictionary
-    # lookup result of f(x) for each item in list
-    # add result into dictionary: bind x to result
+    # build simple solutions dictionary
+    for num in nums:
+        simple_solns[num] = f(num)
+    
+    # build addition solutions dictionary 
+    for a in nums:
+        for b in nums:
+            ab = (a, b)
+            add_solns[ab] = simple_solns[a] + simple_solns[b]
+            
+    # build subtraction solutions dictionary
+    for c in nums:
+        for d in nums:
+            cd = (c, d)
+            sub_solns[cd] = simple_solns[c] - simple_solns[d]
+            
+    
+    # find matches
+    for i in add_solns.items():
+        for j in sub_solns.items():
+            if i[1] == j[1]:
+                matches.append(i[0] + j[0])
+                
+    for nums in matches:
+        print(f'a:{nums[0]}, b:{nums[1]}, c:{nums[2]}, d:{nums[3]}')
 
-    # while True:
-    #
+sumdiff(q)
+                
+    
+            
+    
